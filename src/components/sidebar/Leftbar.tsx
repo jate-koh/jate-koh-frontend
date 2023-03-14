91f5640fa9f3dbacbf80258bc2d75db4c3559c8e
jate-koh <jate_ben@hotmail.com>
2023-03-14 18:17:09 +0700

    [feat] add flowbite sample sidebar

---
+++ src/components/sidebar/Leftbar.tsx
@@ -0,0 +1,35 @@
+import { Sidebar } from 'flowbite-react';
+
+function Leftbar() {
+    return (
+        <div className="w-fit">
+            <Sidebar aria-label="Default sidebar example">
+                <Sidebar.Items>
+                    <Sidebar.ItemGroup>
+                        <Sidebar.Item href="#">
+                        Dashboard
+                        </Sidebar.Item>
+                        <Sidebar.Item href="#" label="Pro" labelColor="alternative">
+                        Kanban
+                        </Sidebar.Item>
+                        <Sidebar.Item href="#" label="3">
+                        Inbox
+                        </Sidebar.Item>
+                        <Sidebar.Item href="#">
+                        Users
+                        </Sidebar.Item>
+                        <Sidebar.Item href="#">Products</Sidebar.Item>
+                        <Sidebar.Item href="#" /* icon={HiArrowSmRight} */ >
+                        Sign In
+                        </Sidebar.Item>
+                        <Sidebar.Item href="#">
+                        Sign Up
+                        </Sidebar.Item>
+                    </Sidebar.ItemGroup>
+                </Sidebar.Items>
+            </Sidebar>
+        </div>
+    );
+}
+
+export default Leftbar;
